84121c1ce3310898571841309ef24c94030885c0
Hakimsabkhi <hakimsabkhi001@gmail.com>
2025-06-27 12:34:34 +0100

    update

--- src/app/(webpage)/[slugCategorie]/page.tsx
+++ src/app/(webpage)/[slugCategorie]/page.tsx
@@ -1,39 +1,25 @@
 // src/app/(webpage)/[slugCategorie]/page.tsx
 
-import Banner from "@/components/Banner";
-import { fetchData } from "@/lib/fetchData";
-
 
 export const revalidate = 60;
 
-interface CategorieData {
-  _id?: string;        
-  name?: string | null;
-  slug?: string | null;
-  bannerUrl?: string | null;
-}
+
 
 type PageParams = { slugCategorie: string };
 
 export default async function CategoriePage({
-  params,
+
 }: {
   params: Promise<PageParams>;
 }) {
-  // ⚠️ await the lazy params before destructuring
-  const { slugCategorie } = await params;
 
-  /* -------- catégorie -------- */
-  const categorie: CategorieData = await fetchData<CategorieData>(
-    `NavMenu/categorieSubCategoriePage/${slugCategorie}`
-  ).catch(() => ({} as CategorieData));
+
+
 
 
   return (
     <div className="flex flex-col gap-[24px]">
-      {categorie.name && categorie.bannerUrl && (
-        <Banner title={categorie.name} imageBanner={categorie.bannerUrl} />
-      )}
+hello
     </div>
   );
 }
