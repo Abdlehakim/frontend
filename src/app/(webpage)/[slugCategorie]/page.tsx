eaac88feaeb9d7665ff166cc319878dd3587f223
Hakimsabkhi <hakimsabkhi001@gmail.com>
2025-06-28 16:05:03 +0100

    update

--- src/app/(webpage)/[slugCategorie]/page.tsx
+++ src/app/(webpage)/[slugCategorie]/page.tsx
@@ -1,10 +1,18 @@
 // src/app/(webpage)/[slugCategorie]/page.tsx
-
+import Banner from "@/components/Banner";
 import ProductSectionCategoriePage from "@/components/product/categorie/ProductSectionCategoriePage";
 import { fetchData } from "@/lib/fetchData";
 import { SubCategorie } from "@/types/Product";
 
 export const revalidate = 60;
+
+interface CategorieData {
+  _id?: string;
+  name?: string | null;
+  slug?: string | null;
+  bannerUrl?: string | null;
+}
+
 
 type PageParams = { slugCategorie: string };
 
@@ -15,12 +23,23 @@ export default async function CategoriePage({
 }) {
   const { slugCategorie } = await params;
 
+  
+  /* -------- catégorie -------- */
+  const categorie: CategorieData = await fetchData<CategorieData>(
+    `NavMenu/categorieSubCategoriePage/${slugCategorie}`
+  ).catch(() => ({} as CategorieData));
+
+
   const initialSubcategories: SubCategorie[] = await fetchData<SubCategorie[]>(
     `NavMenu/categorieSubCategoriePage/categorie/${slugCategorie}`
   ).catch(() => []);
 
   return (
     <div className="flex flex-col gap-6">
+      {categorie.name && categorie.bannerUrl && (
+        <Banner title={categorie.name} imageBanner={categorie.bannerUrl} />
+      )}
+
       <ProductSectionCategoriePage
         slugCategorie={slugCategorie}
         initialSubcategories={initialSubcategories}
