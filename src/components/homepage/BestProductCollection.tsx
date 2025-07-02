7e1266c9fe9fe23b1b9c4e89cbe8f5ce1a8af6ef
Hakimsabkhi <hakimsabkhi001@gmail.com>
2025-07-02 12:18:29 +0100

    update

--- src/components/homepage/BestProductCollection.tsx
+++ src/components/homepage/BestProductCollection.tsx
@@ -29,13 +29,13 @@ export default async function ProductsBestCollection() {
   return (
     <section className="desktop max-lg:w-[95%] flex flex-col items-center gap-8 py-8">
       <div className="flex w-full flex-col items-center gap-2 relative">
-        <h2 className="font-bold text-2xl text-HomePageTitles capitalize">
+        <h2 className="font-bold text-2xl text-HomePageTitles capitalize text-center max-md:text-lg">
           {header.HPBestCollectionTitle}
         </h2>
-        <p className="text-base text-[#525566]">
+        <p className="text-base text-[#525566] max-md:text-sm">
           {header.HPBestCollectionSubTitle}
         </p>
-        <div className="absolute w-full flex justify-end">
+        <div className="absolute max-lg:justify-center max-lg:static w-full flex justify-end mt-2">
           <Link
             href="/bestproductcollection"
             className="group inline-flex items-center border-2 border-secondary text-secondary font-semibold uppercase tracking-wide px-6 py-2 rounded-full transition-colors duration-200 ease-in-out hover:bg-secondary hover:text-white"
