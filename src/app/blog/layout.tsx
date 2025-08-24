819316da89150a8b14f4d2e9ea0f3993f72d750e
Hakimsabkhi <hakimsabkhi001@gmail.com>
2025-08-24 13:43:36 +0100

    update

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,13 +1,20 @@
-
 import HeaderBlog from "@/components/menu/blog/HeaderBlog";
 import Footer from "@/components/menu/Footer";
+import Providers from "@/components/Providers";
+import GoogleIdentityLoader from "@/components/GoogleIdentityLoader";
+import ClientShell from "@/components/ClientShell";
 
 const SubLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col" >
-        <HeaderBlog />
-        {children}
-        <Footer />
+    <div className="flex flex-col">
+      <Providers>
+        <ClientShell>
+          <HeaderBlog />
+          {children}
+          <Footer />
+        </ClientShell>
+      </Providers>
+      <GoogleIdentityLoader />
     </div>
   );
 };
