7423e378cf3a41c312d79a03e76cbab5735bb688
Hakimsabkhi <hakimsabkhi001@gmail.com>
2025-09-23 17:51:48 +0100

    update

--- src/components/checkout/CheckoutNav.tsx
+++ src/components/checkout/CheckoutNav.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiOutlineCheckCircle } from "react-icons/ai"; // ← NEW
+import { AiOutlineCheckCircle } from "react-icons/ai";
 
 interface CheckoutNavProps {
   currentStep: "cart" | "checkout" | "order-summary";
@@ -9,7 +9,7 @@ const CheckoutNav: React.FC<CheckoutNavProps> = ({ currentStep }) => {
   return (
     <div className="w-full h-[80px] flex justify-center">
       <ol className="items-center justify-center flex w-full max-w-2xl text-center text-sm font-medium text-gray-500">
-        {/* Cart Step */}
+        {/* Étape Panier */}
         <li
           className={`after:border-1 flex items-center after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 sm:after:inline-block sm:after:content-[''] md:w-full xl:after:mx-10 ${
             currentStep === "cart"
@@ -19,11 +19,11 @@ const CheckoutNav: React.FC<CheckoutNavProps> = ({ currentStep }) => {
         >
           <span className="flex items-center after:mx-2 after:text-gray-200 after:content-['/'] dark:after:text-gray-500 sm:after:hidden">
             <AiOutlineCheckCircle className="me-2 h-4 w-4 sm:h-5 sm:w-5" />
-            Cart
+            Panier
           </span>
         </li>
 
-        {/* Checkout Step */}
+        {/* Étape Paiement */}
         <li
           className={`after:border-1 flex items-center after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 sm:after:inline-block sm:after:content-[''] md:w-full xl:after:mx-10 ${
             currentStep === "checkout"
@@ -33,11 +33,11 @@ const CheckoutNav: React.FC<CheckoutNavProps> = ({ currentStep }) => {
         >
           <span className="flex items-center after:mx-2 after:text-gray-200 after:content-['/'] dark:after:text-gray-500 sm:after:hidden">
             <AiOutlineCheckCircle className="me-2 h-4 w-4 sm:h-5 sm:w-5" />
-            Checkout
+            Paiement
           </span>
         </li>
 
-        {/* Order Summary Step */}
+        {/* Étape Récapitulatif */}
         <li
           className={`flex shrink-0 items-center ${
             currentStep === "order-summary"
@@ -46,7 +46,7 @@ const CheckoutNav: React.FC<CheckoutNavProps> = ({ currentStep }) => {
           }`}
         >
           <AiOutlineCheckCircle className="me-2 h-4 w-4 sm:h-5 sm:w-5" />
-          Order summary
+          Récapitulatif de commande
         </li>
       </ol>
     </div>
